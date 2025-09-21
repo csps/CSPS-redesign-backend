f252ae2ad5472306449b1561898f256bb1506040
roginand <roginandv@gmail.com>
2025-09-21 14:17:34 +0800

    feat(mapper): add purchase mappers

---
+++ src/main/java/org/csps/backend/mapper/PurchaseMapper.java
@@ -0,0 +1,14 @@
+package org.csps.backend.mapper;
+
+import org.csps.backend.domain.dtos.response.PurchaseResponseDTO;
+import org.csps.backend.domain.entities.Purchase;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring", uses = {PurchaseItemMapper.class})
+public interface PurchaseMapper {
+    @Mapping(target = "studentId", source = "student.studentId")
+    @Mapping(target = "purchaseItemId", ignore = true)
+    @Mapping(target = "items", source = "items")
+    PurchaseResponseDTO toResponseDTO(Purchase purchase);
+}
