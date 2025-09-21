75491b0af27befcd69b5087c57f10e86c7a06176
roginand <roginandv@gmail.com>
2025-09-21 15:15:58 +0800

    fix(controller): update MerchController implementation

--- src/main/java/org/csps/backend/controller/MerchController.java
+++ src/main/java/org/csps/backend/controller/MerchController.java
@@ -6,6 +6,7 @@ import org.csps.backend.domain.dtos.request.MerchRequestDTO;
 import org.csps.backend.domain.dtos.response.MerchResponseDTO;
 import org.csps.backend.service.MerchService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,18 +24,21 @@ public class MerchController {
     private final MerchService merchService;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<MerchResponseDTO> createMerch(@RequestBody MerchRequestDTO merchRequestDTO) {
         MerchResponseDTO createdMerch = merchService.createMerch(merchRequestDTO);
         return ResponseEntity.ok(createdMerch);
     }
 
     @GetMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT')")
     public ResponseEntity<List<MerchResponseDTO>> getAllMerch() {
         List<MerchResponseDTO> merchList = merchService.getAllMerch();
         return ResponseEntity.ok(merchList);
     }
 
     @GetMapping("/{id}")
+    @PreAuthorize("hasRole('ADMIN') or hasRole('STUDENT')")
     public ResponseEntity<MerchResponseDTO> getMerchById(@PathVariable Long id) {
         MerchResponseDTO merch = merchService.getMerchById(id);
         return ResponseEntity.ok(merch);
