0677ae80db2fcb5d17184f92a7a671ea0c2f8b03
roginand <roginandv@gmail.com>
2025-09-21 12:24:13 +0800

    feat(exception): added merch related exceptions

---
+++ src/main/java/org/csps/backend/exception/OutOfStockException.java
@@ -0,0 +1,7 @@
+package org.csps.backend.exception;
+
+public class OutOfStockException extends RuntimeException {
+    public OutOfStockException(String message) {
+        super(message);
+    }
+}
