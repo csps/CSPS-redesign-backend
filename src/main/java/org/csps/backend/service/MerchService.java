0abcbdb9c9380f07494b437ff412c936702b1963
roginand <roginandv@gmail.com>
2025-09-24 16:11:40 +0800

    feat(service): added the put and patch method for the service layer

--- src/main/java/org/csps/backend/service/MerchService.java
+++ src/main/java/org/csps/backend/service/MerchService.java
@@ -2,12 +2,16 @@
 package org.csps.backend.service;
 
 import java.util.List;
+import java.util.Map;
 
 import org.csps.backend.domain.dtos.request.MerchRequestDTO;
+import org.csps.backend.domain.dtos.request.MerchUpdateRequestDTO;
 import org.csps.backend.domain.dtos.response.MerchResponseDTO;
 
 public interface MerchService {
     MerchResponseDTO createMerch(MerchRequestDTO request);
     List<MerchResponseDTO> getAllMerch();
     MerchResponseDTO getMerchById(Long id);
+    Map<String, Object> putMerch(Long merchId, MerchUpdateRequestDTO merchUpdateRequestDTO);
+    Map<String, Object> patchMerch(Long merchId, MerchUpdateRequestDTO merchUpdateRequestDTO);
 }
--- src/main/java/org/csps/backend/service/impl/MerchServiceImpl.java
+++ src/main/java/org/csps/backend/service/impl/MerchServiceImpl.java
@@ -1,12 +1,19 @@
  package org.csps.backend.service.impl;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
+import org.csps.backend.domain.dtos.request.InvalidRequestException;
+import org.csps.backend.domain.dtos.request.MerchAlreadyExistException;
 import org.csps.backend.domain.dtos.request.MerchRequestDTO;
+import org.csps.backend.domain.dtos.request.MerchUpdateRequestDTO;
 import org.csps.backend.domain.dtos.response.MerchResponseDTO;
 import org.csps.backend.domain.dtos.response.MerchVariantResponseDTO;
 import org.csps.backend.domain.entities.Merch;
 import org.csps.backend.domain.entities.MerchVariant;
+import org.csps.backend.domain.enums.MerchType;
+import org.csps.backend.exception.MerchNotFoundException;
 import org.csps.backend.mapper.MerchMapper;
 import org.csps.backend.mapper.MerchVariantMapper;
 import org.csps.backend.repository.MerchRepository;
@@ -73,5 +80,82 @@ public class MerchServiceImpl implements MerchService {
         Merch merch = merchRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Merch not found with id: " + id));
         return merchMapper.toResponseDTO(merch);
+    }
+
+    @Override
+    @Transactional
+    public Map<String, Object> putMerch(Long merchId, MerchUpdateRequestDTO merchUpdateRequestDTO) {
+        // find the merch by id
+        Merch foundMerch = merchRepository.findById(merchId)
+                            .orElseThrow(() -> new MerchNotFoundException("Merch Not Found"));
+        
+        // get the new values
+        String newMerchName = merchUpdateRequestDTO.getMerchName();
+        String newMerchDescription = merchUpdateRequestDTO.getDescription();
+        MerchType newMerchType = merchUpdateRequestDTO.getMerchType();
+        
+        // Validate required fields
+        if (newMerchName == null || newMerchName.trim().isEmpty() 
+            || newMerchDescription == null || newMerchDescription.trim().isEmpty() 
+            || newMerchType == null) {
+            throw new InvalidRequestException("Invalid Credential");
+        }
+    
+        // check if the merch name is already exist
+        if (!foundMerch.getMerchName().equals(newMerchName) 
+            && merchRepository.existsByMerchName(newMerchName)) {
+            throw new MerchAlreadyExistException("Merch Name Already Exist");
+        }
+        
+        // set the new values
+        foundMerch.setMerchName(newMerchName);
+        foundMerch.setMerchType(newMerchType);
+        foundMerch.setDescription(newMerchDescription);
+
+
+        // save the merch
+        merchRepository.save(foundMerch);
+        
+        // return the response
+
+        return Map.of("message", "Merch Updated Successfully",
+                        "timestamp", LocalDateTime.now(),
+                        "status", 200);    
+        }
+
+    @Override
+    @Transactional
+    public Map<String, Object> patchMerch(Long merchId, MerchUpdateRequestDTO merchUpdateRequestDTO) {
+        // find the merch by id
+        Merch foundMerch = merchRepository.findById(merchId)
+                            .orElseThrow(() -> new MerchNotFoundException("Merch Not Found"));
+        
+
+        // get the new values
+        String newMerchName = merchUpdateRequestDTO.getMerchName();
+        String newMerchDescription = merchUpdateRequestDTO.getDescription();
+        MerchType newMerchType = merchUpdateRequestDTO.getMerchType();
+        
+
+        // set the new values
+        if (newMerchName != null && !newMerchName.trim().isEmpty()) {
+            foundMerch.setMerchName(newMerchName);
+        }
+        if (newMerchDescription != null && !newMerchDescription.trim().isEmpty()) {
+            foundMerch.setDescription(newMerchDescription);
+        }
+        if (newMerchType != null && !newMerchType.toString().isEmpty()) {
+            foundMerch.setMerchType(newMerchType);
+        }
+
+
+        // save the merch
+        merchRepository.save(foundMerch);
+
+
+        // return the response
+        return Map.of("message", "Merch Updated Successfully",
+                        "timestamp", LocalDateTime.now(),
+                        "status", 200);
     }
 }
