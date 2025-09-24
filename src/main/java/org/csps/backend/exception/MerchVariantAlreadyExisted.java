1e104f6f76c805d7b11ab7da7eac622f23cd6830
roginand <roginandv@gmail.com>
2025-09-25 07:53:33 +0800

    feat(exception): add MerchVariantAlreadyExisted exception

---
+++ src/main/java/org/csps/backend/exception/MerchVariantAlreadyExisted.java
@@ -0,0 +1,8 @@
+package org.csps.backend.exception;
+
+public class MerchVariantAlreadyExisted extends RuntimeException {
+    public MerchVariantAlreadyExisted(String message) {
+        super(message);
+    }
+
+}
