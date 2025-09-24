7be25af6594b1c611c9eddabe7c9e9f63d39ffd5
roginand <roginandv@gmail.com>
2025-09-24 16:12:06 +0800

    feat(controller): added put and patch endpoint for controller

--- src/main/java/org/csps/backend/controller/MerchController.java
+++ src/main/java/org/csps/backend/controller/MerchController.java
@@ -1,15 +1,19 @@
 package org.csps.backend.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.csps.backend.domain.dtos.request.MerchRequestDTO;
+import org.csps.backend.domain.dtos.request.MerchUpdateRequestDTO;
 import org.csps.backend.domain.dtos.response.MerchResponseDTO;
 import org.csps.backend.service.MerchService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -42,6 +46,20 @@ public class MerchController {
     public ResponseEntity<MerchResponseDTO> getMerchById(@PathVariable Long id) {
         MerchResponseDTO merch = merchService.getMerchById(id);
         return ResponseEntity.ok(merch);
+    }
+
+    @PutMapping("/update/{merchId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String,Object>> putMerch(@PathVariable Long merchId, @RequestBody MerchUpdateRequestDTO merchUpdateRequestDTO) {
+        Map<String,Object> response = merchService.putMerch(merchId, merchUpdateRequestDTO);
+        return ResponseEntity.ok(response);
+    }
+
+    @PatchMapping("/update/{merchId}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Map<String,Object>> patchMerch(@PathVariable Long merchId, @RequestBody MerchUpdateRequestDTO merchUpdateRequestDTO) {
+        Map<String,Object> response = merchService.patchMerch(merchId, merchUpdateRequestDTO);
+        return ResponseEntity.ok(response);
     }
     
 }
