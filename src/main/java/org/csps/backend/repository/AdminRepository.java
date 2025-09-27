ed094f4f0ccc10c44e54db70a1dc75c5f2b12987
roginand <roginandv@gmail.com>
2025-09-27 18:43:29 +0800

    feat(repository): update AdminRepository and UserAccountRepository, remove typo file

--- src/main/java/org/csps/backend/repository/AdminRepository.java
+++ src/main/java/org/csps/backend/repository/AdminRepository.java
@@ -3,8 +3,11 @@ package org.csps.backend.repository;
 import java.util.Optional;
 
 import org.csps.backend.domain.entities.Admin;
+import org.csps.backend.domain.enums.AdminPosition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
     Optional<Admin> findByUserAccountUserAccountId(Long userAccountId);
+    boolean existsByPosition(AdminPosition position);
+
 }
--- src/main/java/org/csps/backend/repository/UserAccountRepository.java
+++ src/main/java/org/csps/backend/repository/UserAccountRepository.java
@@ -1,9 +1,19 @@
 package org.csps.backend.repository;
 
+import java.util.Optional;
 
 import org.csps.backend.domain.entities.UserAccount;
+import org.csps.backend.domain.entities.UserProfile;
+import org.csps.backend.domain.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     UserAccount findByUsername(String username);
+    boolean existsByUsername(String username);
+    Optional<UserAccount> findByUserProfile(UserProfile userProfile);
+    Optional<UserAccount> findByUserProfileAndRole(UserProfile userProfile, UserRole role);
+
+    
 }
