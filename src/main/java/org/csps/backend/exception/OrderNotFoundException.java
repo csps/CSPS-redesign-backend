f9f97fd9e52ccd4f3e7b272e3a8f576e05e12470
roginand <roginandv@gmail.com>
2025-10-06 15:10:21 +0800

    feat(exception): add OrderNotFoundException for order not found scenarios

---
+++ src/main/java/org/csps/backend/exception/OrderNotFoundException.java
@@ -0,0 +1,8 @@
+package org.csps.backend.exception;
+
+public class OrderNotFoundException extends RuntimeException {
+    public OrderNotFoundException(String message) {
+        super(message);
+    }
+
+}
