36f060aa69284c30676a578d66b999b70512cbf3
roginand <roginandv@gmail.com>
2026-01-12 12:47:41 +0800

    feat(mapper): added orderItemMapper and orderMapper

---
+++ src/main/java/org/csps/backend/mapper/OrderItemMapper.java
@@ -0,0 +1,27 @@
+package org.csps.backend.mapper;
+
+import org.csps.backend.domain.dtos.request.OrderItemRequestDTO;
+import org.csps.backend.domain.dtos.response.OrderItemResponseDTO;
+import org.csps.backend.domain.entities.OrderItem;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface OrderItemMapper {
+
+    @Mapping(source = "order.orderId", target = "orderId")
+    @Mapping(source = "merchVariantItem.merchVariant.merch.merchName", target = "merchName")
+    @Mapping(source = "merchVariantItem.merchVariant.color", target = "color")
+    @Mapping(source = "merchVariantItem.merchVariant.design", target = "design")
+    @Mapping(source = "merchVariantItem.size", target = "size")
+    @Mapping(source = "merchVariantItem.merchVariant.s3ImageKey", target = "s3ImageKey")
+    @Mapping(source = "quantity", target = "quantity")
+    @Mapping(target = "totalPrice", expression = "java(orderItem.getQuantity() * orderItem.getPriceAtPurchase())")
+    @Mapping(source = "createdAt", target = "createdAt")
+    @Mapping(source = "updatedAt", target = "updatedAt")
+    OrderItemResponseDTO toResponseDTO(OrderItem orderItem);
+
+    @Mapping(source="merchVariantItemId", target="merchVariantItem.merchVariantItemId")
+    @Mapping(source="orderId", target="order.orderId")
+    OrderItem toEntity(OrderItemRequestDTO orderItemRequestDTO);
+}
--- src/main/java/org/csps/backend/mapper/OrderMapper.java
+++ src/main/java/org/csps/backend/mapper/OrderMapper.java
@@ -1,39 +1,31 @@
     package org.csps.backend.mapper;
 
-    import org.csps.backend.domain.dtos.request.OrderPostRequestDTO;
-    import org.csps.backend.domain.dtos.response.OrderResponseDTO;
-    import org.csps.backend.domain.entities.Order;
-    import org.mapstruct.Mapper;
-    import org.mapstruct.Mapping;
+import org.csps.backend.domain.dtos.request.OrderPostRequestDTO;
+import org.csps.backend.domain.dtos.response.OrderResponseDTO;
+import org.csps.backend.domain.entities.Order;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 
-    @Mapper(componentModel = "spring")
-    public interface OrderMapper {
+@Mapper(componentModel = "spring", uses = OrderItemMapper.class)
+public interface OrderMapper {
 
-        @Mapping(source = "orderId", target = "orderId")
-        @Mapping(source = "merchVariantItem.merchVariant.merch.merchName", target = "merchVariantName")
-        @Mapping(source = "merchVariantItem.merchVariant.color", target = "merchVariantColor")
-        @Mapping(source = "merchVariantItem.size", target = "merchVariantSize")
-        @Mapping(source = "quantity", target = "quantity")
-        @Mapping(target = "studentName", expression = "java(getStudentName(order))")
-        @Mapping(source = "totalPrice", target = "totalPrice")
-        @Mapping(source = "orderStatus", target = "orderStatus")
-        OrderResponseDTO toResponseDTO(Order order);
+    @Mapping(target = "studentName", expression = "java(getStudentName(order))")
+    OrderResponseDTO toResponseDTO(Order order);
 
-        @Mapping(target = "merchVariantItem", ignore = true)
-        @Mapping(source = "quantity", target = "quantity")
-        @Mapping(target = "student", ignore = true)
-        @Mapping(target = "orderDate", ignore = true)
-        @Mapping(target = "totalPrice", ignore = true)
-        @Mapping(target = "orderId", ignore = true)
-        @Mapping(target = "orderStatus", ignore = true)
-        Order toEntity(OrderPostRequestDTO orderPostRequestDTO);
+    @Mapping(target = "orderItems", ignore = true)
+    @Mapping(source = "quantity", target = "quantity")
+    @Mapping(target = "student", ignore = true)
+    @Mapping(target = "orderDate", ignore = true)
+    @Mapping(target = "totalPrice", ignore = true)
+    @Mapping(target = "orderId", ignore = true)
+    Order toEntity(OrderPostRequestDTO orderPostRequestDTO);
 
-        default String getStudentName(Order order) {
-            var profile = order.getStudent().getUserAccount().getUserProfile();
-            String middle = profile.getMiddleName();
-            String middleInitial = (middle == null || middle.isEmpty()) ? "" : middle.charAt(0) + ". ";
-            return profile.getFirstName() + " " + middleInitial + profile.getLastName();
-        }
-        
+    default String getStudentName(Order order) {
+        var profile = order.getStudent().getUserAccount().getUserProfile();
+        String middle = profile.getMiddleName();
+        String middleInitial = (middle == null || middle.isEmpty()) ? "" : middle.charAt(0) + ". ";
+        return profile.getFirstName() + " " + middleInitial + profile.getLastName();
     }
+    
+}
