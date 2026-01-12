05ec18eb2e107d891f9cc46dc37de1e2511ec318
roginand <roginandv@gmail.com>
2026-01-12 13:11:01 +0800

    feat(order): added order repository

--- src/main/java/org/csps/backend/repository/OrderRepository.java
+++ src/main/java/org/csps/backend/repository/OrderRepository.java
@@ -3,16 +3,14 @@ package org.csps.backend.repository;
 import java.util.List;
 
 import org.csps.backend.domain.entities.Order;
-import org.csps.backend.domain.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
+    
     @Query("SELECT o FROM Order o WHERE o.student.studentId = :studentId")
-    public List<Order> findByStudentId(String studentId);
-
-    @Query("SELECT o FROM Order o WHERE o.orderStatus = :orderStatus")
-    public List<Order> findByOrderStatus(OrderStatus orderStatus);
+    List<Order> findByStudentId(String studentId);
 }
+
