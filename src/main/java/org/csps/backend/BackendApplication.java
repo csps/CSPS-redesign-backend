079374a705ec198c41b6c4cfe1f5cf6a9a2bfb1e
roginand <roginandv@gmail.com>
2026-02-17 10:09:37 +0800

    fix: enable AspectJ proxy and adjust JWT token expiration

--- src/main/java/org/csps/backend/BackendApplication.java
+++ src/main/java/org/csps/backend/BackendApplication.java
@@ -2,10 +2,12 @@ package org.csps.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
+@EnableAspectJAutoProxy
 public class BackendApplication {
 
 	public static void main(String[] args) {
