2d06965b38f8ae7a41fb0ba4312b49572d835674
roginand <roginandv@gmail.com>
2026-02-18 11:46:06 +0800

    feat: add and update repositories and specifications

--- src/main/java/org/csps/backend/repository/OrderRepository.java
+++ src/main/java/org/csps/backend/repository/OrderRepository.java
@@ -8,17 +8,20 @@ import org.csps.backend.domain.enums.OrderStatus;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderRepository extends JpaRepository<Order, Long>, JpaSpecificationExecutor<Order> {
     
     @Query("SELECT o FROM Order o WHERE o.student.studentId = :studentId")
     List<Order> findByStudentId(String studentId);
     
     @Query("SELECT o FROM Order o WHERE o.student.studentId = :studentId ORDER BY o.orderDate DESC")
     Page<Order> findByStudentId(String studentId, Pageable pageable);
+
+    
 
     Page<Order> findAllByOrderByOrderDateDesc(Pageable pageable);
     
