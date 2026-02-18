27c09049a5133380f69878a9193e014c0a77f78c
roginand <roginandv@gmail.com>
2026-02-18 11:46:47 +0800

    chore: update configuration, infrastructure, and add scheduling

--- src/main/java/org/csps/backend/BackendApplication.java
+++ src/main/java/org/csps/backend/BackendApplication.java
@@ -3,11 +3,13 @@ package org.csps.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
 @CrossOrigin(origins = "*")
 @EnableAspectJAutoProxy
+@EnableScheduling
 public class BackendApplication {
 
 	public static void main(String[] args) {
