39c2c6303eb7c04673f0040c9b3489748f09aa3c
roginand <roginandv@gmail.com>
2026-02-20 21:48:56 +0800

    refactor: change email verification relationship to ManyToOne

--- src/main/java/org/csps/backend/domain/entities/EmailVerification.java
+++ src/main/java/org/csps/backend/domain/entities/EmailVerification.java
@@ -10,7 +10,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -35,7 +35,7 @@ public class EmailVerification {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long emailVerificationId;
     
-    @OneToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_account_id", nullable = false)
     private UserAccount userAccount;
     
