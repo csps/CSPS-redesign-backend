6c08fbaedc3ac6f53ca73231ce91c7bd0c87c1cc
roginand <roginandv@gmail.com>
2026-02-21 12:41:47 +0800

    feat(cart): membership merch will not appear in the merch list if the membership is in the cart

--- src/main/java/org/csps/backend/repository/CartItemRepository.java
+++ src/main/java/org/csps/backend/repository/CartItemRepository.java
@@ -2,10 +2,19 @@ package org.csps.backend.repository;
 
 import org.csps.backend.domain.entities.CartItem;
 import org.csps.backend.domain.entities.composites.CartItemId;
+import org.csps.backend.domain.enums.MerchType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CartItemRepository extends JpaRepository<CartItem, CartItemId>{
-
+    
+    @Query("SELECT COUNT(ci) > 0 FROM CartItem ci " +
+           "JOIN ci.merchVariantItem mvi " +
+           "JOIN mvi.merchVariant mv " +
+           "JOIN mv.merch m " +
+           "WHERE ci.cart.student.studentId = :studentId AND m.merchType = :merchType")
+    boolean existsByStudentIdAndMerchType(@Param("studentId") String studentId, @Param("merchType") MerchType merchType);
 }
